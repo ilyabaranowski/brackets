d3ff61bf4e9202bd215d5f3bc937909d13c0f4d2
iliuxa <iliuxa32@gmail.com>
2023-01-29 19:47:31 +0300

    fix: task compl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = bracketsConfig.flat();
+  const obj = {};
+  const stack = [];
+
+  for (x = 0; x < arr.length; x+=2) {
+    obj[arr[x+1]] = arr [x]
+  }
+
+  for (y = 0; y < str.length; y++) {
+    const top = stack[stack.length - 1];
+    if (obj[str[y]] === top && stack.length >= 1) {
+      stack.pop();
+    }
+
+    else if (Object.values(obj).includes(str[y])) {
+      stack.push(str[y])
+    }
+
+    else {return false};
+  }
+
+  return  stack.length === 0;
 }
